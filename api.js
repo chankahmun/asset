66d44fdaddce241a53ac0b8bcfd64e0631cef6a5
chankahmun <45312550+chankahmun@users.noreply.github.com>
2022-01-09 14:15:58 +0800

    Update api.js

--- api.js
+++ api.js
@@ -2,6 +2,10 @@
    "data": {
       "api": "https://financialmodelingprep.com/",
       "key": "0f0913e59fe727e1fd0432b590f0a3d4"
+   },
+   "news":{
+	  "api": "https://newsapi.org/",
+	  "key": "87811770b7bd4d7e9c092aaf880d5633"
    }
 	 
 }
